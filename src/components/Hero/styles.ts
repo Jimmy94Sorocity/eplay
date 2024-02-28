c350ce2c0fe60239b1691f228e15572cc44a6f09
Matheus Fré <matheusfre94@gmail.com>
2024-02-28 10:51:14 -0300

    finalizando o hero

--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import { cores } from '../../styles'
+import { TagContainer } from '../Tag/styles'
 
 export const Banner = styled.div`
   position: relative;
@@ -10,6 +12,8 @@ export const Banner = styled.div`
   background-position: center;
   background-size: 100%;
 
+  padding-top: 16px;
+
   &::after {
     position: absolute;
     background-color: #000;
@@ -19,5 +23,39 @@ export const Banner = styled.div`
     left: 0;
     content: '';
     opacity: 0.56;
+  }
+
+  ${TagContainer} {
+    margin-right: 8px;
+  }
+
+  .container {
+    z-index: 1;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    justify-content: space-between;
+  }
+`
+
+export const Infos = styled.div`
+  padding: 16px;
+  background-color: ${cores.preta};
+  max-width: 290px;
+  font-weight: bold;
+
+  h2 {
+    font-size: 32px;
+  }
+
+  p {
+    font-size: 18px;
+    margin: 16px 0;
+
+    span {
+      display: block;
+      text-decoration: line-through;
+    }
   }
 `
